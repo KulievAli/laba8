2611032471f174b78f7a6c3e96d8853cc8f7d3ca
Али <k.u.l.i.e.v@mail.ru>
2021-03-31 18:15:02 +0300

    laba 9

--- kuliev/MyLog.php
+++ kuliev/MyLog.php
@@ -6,17 +6,30 @@ use core\LogAbstract;
 use core\LogInterface;
 
 class MyLog extends LogAbstract implements LogInterface {
-    public static function log($str) {
-        array_push(LogAbstract::Instance()->log, $str);
-    }
+    public function _write()
+    {
+        $d = new \DateTime();
+        $date = $d->format('d.m.Y_H.i.s.ms');
+        $logFileName = "log/$date.log";
 
-    public static function write() {
-        LogAbstract::Instance()->_write();
-    }
-
-    public function _write() {
-        foreach (LogAbstract::Instance()->log as $value) {
-            echo $value;
+        if (!file_exists("log")) {
+            mkdir("log");
         }
+
+        foreach ($this->log as $mass) {
+            echo $mass . PHP_EOL;
+        }
+
+        file_put_contents($logFileName, implode("\n", $this->log));
+    }
+
+    public static function log($str):  void
+    {
+        self::Instance()->log[] = $str;
+    }
+
+    public static function write(): void
+    {
+        self::Instance()->_write();
     }
 }
